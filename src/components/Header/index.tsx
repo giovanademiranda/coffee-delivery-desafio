91f015cbdec5145d061ac120673f2af36566530f
Giovana de Miranda <giovana.miranda@bitize.com.br>
2023-12-11 15:43:16 -0300

    feat add função para exibir total de itens

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,18 @@
 'use client'
 
+import { CartContext } from '@/context/CartContext';
 import { MapPin, ShoppingCart } from '@phosphor-icons/react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useContext } from 'react';
 import Logo from '../../assets/logo.svg';
 
 export default function Header() {
+  // Acessa o contexto do carrinho
+  const { cart } = useContext(CartContext);
+
+  // Calcula o total de itens no carrinho
+  const totalItems = cart.reduce((total, product) => total + product.quantity, 0);
   return (
     <header className="flex max-w-7xl justify-between items-center p-5">
       <div className="flex">
@@ -20,7 +27,9 @@ export default function Header() {
         </div>
         <Link href="/checkout" className="flex relative p-2 justify-center items-center gap-1 m-2 rounded-md bg-yellow-light">
           <ShoppingCart size={24} color="#C47F17" weight="fill" />
-          <p className='absolute flex justify-center items-center -top-2 -right-2 w-5 h-5 text-center text-white rounded-full bg-yellow-dark text-sm'>3</p>
+          {totalItems > 0 && (
+            <p className='absolute flex justify-center items-center -top-2 -right-2 w-5 h-5 text-center text-white rounded-full bg-yellow-dark text-sm'>{totalItems}</p>
+          )}
         </Link>
       </nav>
     </header>
