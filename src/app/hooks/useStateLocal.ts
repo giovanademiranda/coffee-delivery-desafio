cda9983c1c9e93045b79b7751c47dc3e23c6f072
Giovana de Miranda <giovana.miranda@bitize.com.br>
2023-12-15 14:52:17 -0300

    fix: useStateLocal do LocalStorage

--- src/app/hooks/useStateLocal.ts
+++ src/app/hooks/useStateLocal.ts
@@ -1,8 +1,46 @@
 import { CartProduct } from "@/context/CartContext";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useCallback, useEffect, useState } from "react";
+
+const isServer = typeof window === 'undefined';
 
 export default function useStateLocal(): [CartProduct[], Dispatch<SetStateAction<CartProduct[]>>] {
-  const [cart, setCart] = useState<CartProduct[]>([]);
+  const [cart, setCartLocal] = useState<CartProduct[]>([])
+  const inicializeCart = () => {
+    if (!isServer) {
+      const storedCart = localStorage.getItem('coffee-delivery-cart');
+      return storedCart ? JSON.parse(storedCart) : [];
+    }
+    return [];
+  }
+
+  useEffect(() => {
+    if (!isServer) setCartLocal(inicializeCart());
+  }, []);
+
+  const setCart: Dispatch<SetStateAction<CartProduct[]>> = useCallback((newCart) => {
+    if (!isServer) {
+      const newCartValue = (newCart instanceof Function) ? newCart(cart) : newCart;
+      localStorage.setItem('coffee-delivery-cart', JSON.stringify(newCartValue));
+      setCartLocal(newCartValue);
+    }
+  }, [setCartLocal, cart]);
+
+  return [cart, setCart];
+}
+
+/*
+const [mounted, setMounted] = useState(false);
+  const [localWrited, setLocalWrited] = useState(false);
+  const [cart, setCart] = useState<CartProduct[]>(() => {
+    if (mounted) {
+      const storedCart = localStorage.getItem('coffee-delivery-cart');
+      setLocalWrited(true);
+      return storedCart ? JSON.parse(storedCart) : [];
+    }
+    return [];
+  });
+
+  useEffect(() => setMounted(true), []);
 
   useEffect(() => {
     const storedCart = localStorage.getItem('coffee-delivery-cart');
@@ -12,9 +50,9 @@ export default function useStateLocal(): [CartProduct[], Dispatch<SetStateAction
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('coffee-delivery-cart', JSON.stringify(cart));
-  }, [cart]);
-
-  return [cart, setCart];
-}
+    if (mounted && localWrited) {
+      localStorage.setItem('coffee-delivery-cart', JSON.stringify(cart));
+    }
+  }, [cart, localWrited, mounted]);
+*/
 
